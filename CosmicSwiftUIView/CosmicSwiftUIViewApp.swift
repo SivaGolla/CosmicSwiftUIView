7eea7feb3e19d9807dbea652bc32840f8ef253f1
Siva Golla <gvsivannarayana@gmail.com>
2024-09-07 17:05:02 +0100

    Created an empty project

---
+++ CosmicSwiftUIView/CosmicSwiftUIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmicSwiftUIViewApp.swift
+//  CosmicSwiftUIView
+//
+//  Created by Venkata Sivannarayana Golla on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CosmicSwiftUIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
